2d52680ba134b61e891cc93731b2bf0f7af33c22
Mike <boxingbat@gmail.com>
2024-02-10 19:50:24 +0800

    setup bones

---
+++ BodyTrackingTrail/BodyTrackingTrail/SkeletonJoint.swift
@@ -0,0 +1,13 @@
+//
+//  SkeletonJoint.swift
+//  BodyTrackingTrail
+//
+//  Created by 1 on 2024/2/10.
+//
+
+import Foundation
+
+struct SkeletonJoint {
+    let name: String
+    var position: SIMD3<Float>
+}
